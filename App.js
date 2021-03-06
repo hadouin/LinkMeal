4e9a20779e45b8f24c829e615eaf9b0cc6b5d51b
Hadouin-Leroy <hleroy@juniorisep.com>
2021-03-06 18:25:59 +0100

    First commit

--- App.js
+++ App.js
@@ -5,8 +5,16 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.container}>
+        <Text>Hello, World!</Text>
+        <Text>Hello, World!</Text>
+        <StatusBar style="auto" />
+      </View>
+      <View style={styles.container}>
+        <image style={styles.image}/>
+        <image style={styles.image}/>
+        <image style={styles.image}/>
+      </View>
     </View>
   );
 }
@@ -14,6 +22,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  Heading: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  menu: {
+    
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
