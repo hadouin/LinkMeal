f590d4048a160b26cdb63d65518b918b03814904
Hadouin-Leroy <hleroy@juniorisep.com>
2021-04-14 23:35:44 +0200

    created wallet

---
+++ app/screens/WalletScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function WalletScreen() {
+  return (
+    <View>
+      <Text>WalletScreen</Text>
+    </View>
+  );
+}
