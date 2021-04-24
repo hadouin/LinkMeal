08929bc1fd6379246e0f247edd243f918705a161
Hadouin-Leroy <hleroy@juniorisep.com>
2021-04-24 22:30:24 +0200

    stopped below zero

--- app/components/CounterComp.js
+++ app/components/CounterComp.js
@@ -16,18 +16,18 @@ const Icon = createIconSetFromIcoMoon(
 );
 
 export default class CounterComp extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+
     this.state = {
       number: 0,
-      count: "100",
+      count: this.props.defaultVal,
       timing: 200,
     };
     this.timer = null;
     this.addOne = this.addOne.bind(this);
     this.delOne = this.delOne.bind(this);
     this.stopTimer = this.stopTimer.bind(this);
-    this.handleChange = this.handleChange.bind(this);
   }
   addOne() {
     this.increment(this.state.count);
@@ -53,39 +53,56 @@ export default class CounterComp extends Component {
     this.setState({
       count: text.replace(/[^0-9]/g, ""),
     });
+    this.props.changeHandler(text);
   }
 
   increment(compte) {
-    var value = parseInt(compte, 10) + 10;
+    var value = parseInt(compte, 10) + this.props.increment;
     let string = value.toString(10);
     this.setState({
       count: string,
     });
+    this.props.changeHandler(string);
   }
   decrement(compte) {
-    var value = parseInt(compte, 10) - 10;
+    var value = parseInt(compte, 10) - this.props.increment;
+    if (value < 0) {
+      value = 0;
+    }
     let string = value.toString(10);
     this.setState({
       count: string,
     });
+    this.props.changeHandler(string);
   }
 
   render() {
     return (
-      <View style={styles.container}>
-        <TouchableOpacity onPressIn={this.delOne} onPressOut={this.stopTimer}>
-          <Icon name={"Arrow---Down-21"} size={40} color={"white"} />
-        </TouchableOpacity>
-        <TextInput
-          style={styles.input}
-          onChangeText={(text) => this.handleChange(text)}
-          keyboardType="numeric"
-          placeholder="Poids de votre plat"
-          value={this.state.count}
-        />
-        <TouchableOpacity onPressIn={this.addOne} onPressOut={this.stopTimer}>
-          <Icon name={"Arrow---Up-21"} size={40} color={"white"} />
-        </TouchableOpacity>
+      <View>
+        <Text
+          style={{
+            paddingLeft: 5,
+            margin: 2,
+            fontFamily: "Montserrat_400Regular",
+          }}
+        >
+          {this.props.title}
+        </Text>
+        <View style={styles.container}>
+          <TouchableOpacity onPressIn={this.delOne} onPressOut={this.stopTimer}>
+            <Icon name={"Arrow---Down-21"} size={40} color={"white"} />
+          </TouchableOpacity>
+          <TextInput
+            style={styles.input}
+            onChangeText={(text) => this.handleChange(text)}
+            keyboardType="numeric"
+            placeholder="Poids de votre plat"
+            value={this.state.count}
+          />
+          <TouchableOpacity onPressIn={this.addOne} onPressOut={this.stopTimer}>
+            <Icon name={"Arrow---Up-21"} size={40} color={"white"} />
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
@@ -93,11 +110,16 @@ export default class CounterComp extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    paddingHorizontal: 10,
+    paddingHorizontal: 15,
     justifyContent: "space-around",
     flexDirection: "row",
     backgroundColor: "#ff8b4b",
     borderRadius: 10,
   },
-  input: { textAlign: "center" },
+  input: {
+    textAlign: "center",
+    fontSize: 19,
+    color: "#fff",
+    marginHorizontal: 20,
+  },
 });
