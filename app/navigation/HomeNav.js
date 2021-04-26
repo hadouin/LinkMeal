3a38ed16c2aabb7b832d8d48fa954a0df76d8f08
Hadouin-Leroy <hleroy@juniorisep.com>
2021-04-26 19:20:27 +0200

    header styles

--- app/navigation/HomeNav.js
+++ app/navigation/HomeNav.js
@@ -4,6 +4,7 @@ import HomeScreen from "../screens/HomeScreen";
 import { StyleSheet, View, Image } from "react-native";
 import DetailScreen from "../screens/DetailScreen";
 import MapScreen from "../screens/MapScreen";
+import { BlurView } from "expo-blur";
 
 const Home = createStackNavigator();
 
@@ -26,8 +27,32 @@ export default function HomeNav(props) {
           headerTitleAlign: "center",
         }}
       />
-      <Home.Screen name="Details" component={DetailScreen} />
-      <Home.Screen name="Map" component={MapScreen} />
+      <Home.Screen
+        name="Details"
+        component={DetailScreen}
+        options={{
+          headerBackTitle: "",
+          headerTransparent: true,
+          title: "",
+          headerStyle: {
+            backgroundColor: "#ff8b4b20",
+            height: 50,
+          },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+        }}
+      />
+      <Home.Screen
+        name="Map"
+        component={MapScreen}
+        options={{
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+        }}
+      />
     </Home.Navigator>
   );
 }
