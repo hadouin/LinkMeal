5dbf5c22b22b6ca38badb5861941fb138b02ae31
Hadouin-Leroy <hleroy@juniorisep.com>
2021-04-27 14:18:14 +0200

    settings Screen

---
+++ app/screens/SettingsScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function SettingsScreen() {
+  return (
+    <View>
+      <Text>Settings Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
