39cc69fba4beb504fc005b550d19f85e1218b188
Hadouin-Leroy <hleroy@juniorisep.com>
2021-04-28 07:27:27 +0200

    detail screen

--- app/navigation/FriendNav.js
+++ app/navigation/FriendNav.js
@@ -2,6 +2,7 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import FriendScreen from "../screens/FriendScreen";
 import FriendInfoScreen from "../screens/FriendInfoScreen";
+import DetailScreen from "../screens/DetailScreen";
 const Friend = createStackNavigator();
 
 export default function FriendNav() {
@@ -9,6 +10,23 @@ export default function FriendNav() {
     <Friend.Navigator initialRouteName="Friends">
       <Friend.Screen name="Friends" component={FriendScreen} />
       <Friend.Screen name="FriendInfo" component={FriendInfoScreen} />
+      <Friend.Screen
+        name="Details"
+        component={DetailScreen}
+        options={{
+          headerBackTitle: "",
+          headerTransparent: true,
+          title: "",
+          headerStyle: {
+            backgroundColor: "#ff8b4b40",
+            height: 50,
+          },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+        }}
+      />
     </Friend.Navigator>
   );
 }
