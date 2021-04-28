9aa8382f361556ef5964ef1c245106a72c9bbd21
Hadouin-Leroy <hleroy@juniorisep.com>
2021-04-28 15:59:51 +0200

    touchable

--- app/components/Author.js
+++ app/components/Author.js
@@ -1,19 +1,28 @@
 import React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 
 export default function Author(props) {
   return (
-    <View style={{ flexDirection: "row" }}>
-      <Image
-        style={{
-          borderRadius: 32,
-        }}
-        source={{ height: 32, width: 32, uri: props.author.picture }}
-      />
-      <Text style={{ alignSelf: "center", marginLeft: 5, color: "#222" }}>
-        {props.author.name.first + " " + props.author.name.last}
-      </Text>
-    </View>
+    <TouchableOpacity
+      style={{
+        padding: 5,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+      onPress={() => props.navigation.navigate("Info", { item: props.author })}
+    >
+      <View style={{ flexDirection: "row" }}>
+        <Image
+          style={{
+            borderRadius: 32,
+          }}
+          source={{ height: 32, width: 32, uri: props.author.picture }}
+        />
+        <Text style={{ alignSelf: "center", marginLeft: 5, color: "#222" }}>
+          {props.author.name.first + " " + props.author.name.last}
+        </Text>
+      </View>
+    </TouchableOpacity>
   );
 }
 
